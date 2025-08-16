41310d76c0fdeb485bf16c3e06e9950d90844048
Sijie Fan <sfan0043@student.monash.edu>
2025-08-16 23:46:25 +1000

    Acitvity4

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+
+//import bookstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './style.css'
+
 createApp(App).mount('#app')
