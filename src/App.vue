6d4fa3312e5a0e2a46b6d0d37da105cee51654b9
Sijie Fan <sfan0043@student.monash.edu>
2025-08-26 19:08:12 +1000

    A5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+
+// import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
@@ -11,9 +12,9 @@ import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
   </header>
 
   <main class="main-box">
-    <LibraryRegistrationForm />
+    <!-- <LibraryRegistrationForm /> -->
     <!-- <JSONLab /> -->
-     <!-- <router-view></router-view> -->
+     <router-view></router-view>
   </main>
   </div>
 </template>
@@ -45,4 +46,14 @@ import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
     flex-wrap: wrap;
   }
 } */
+
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  /* background-color: #e0bfbf; */
+  border-radius: 10px;
+}
+
 </style>
